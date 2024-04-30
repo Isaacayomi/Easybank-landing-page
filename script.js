22836752411b4cce66e5108f872b14e0a5bca0c3
Isaac Ayomide <okunlolaa3@outlook.com>
2024-04-30 16:49:41 +0100

    mobile nav completed

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
-const bodyEl = document.querySelector("main");
+const main = document.querySelector("main");
+const body = document.querySelector("body");
 const openNav = document.querySelector(".openIcon");
 const closeNav = document.querySelector(".closeIcon");
 const navLinks = document.querySelector(".nav__links");
 
 navLinks.style.display = "none";
 
+main.classList.remove("addOverlay");
+
 const openMenu = () => {
   navLinks.style.display = "flex";
   openNav.classList.add("hide");
   closeNav.classList.remove("hide");
-  bodyEl.style.overflow = "hidden";
+  main.classList.add("addOverlay");
+  main.style.height = "100%";
+  body.style.overflow = "hidden";
 };
 
 const closeMenu = () => {
-  if ((navLinks.style.display = "flex")) {
+  if (navLinks.style.display === "flex") {
     navLinks.style.display = "none";
     openNav.classList.remove("hide");
     closeNav.classList.add("hide");
-    bodyEl.style.overflow = "scroll";
+    main.classList.remove("addOverlay");
+    body.style.overflow = "scroll";
   }
 };
 
+document.querySelector(".nav__items").addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+    body.style.overflow = "scroll";
+
+    closeMenu();
+  }
+});
+
 openNav.addEventListener("click", openMenu);
 closeNav.addEventListener("click", closeMenu);
-bodyEl.addEventListener("click", closeMenu);
+main.addEventListener("click", closeMenu);
